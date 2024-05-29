0e53905d840048c5e7904259fc1180c2d4f14a63
Raihan Adam <hallwack.id@gmail.com>
2024-05-29 09:44:56 +0700

    feat: create router in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { createBrowserRouter } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import { RouterProvider } from "react-router-dom"
+
 function App() {
+  const router = createBrowserRouter([
+    { path: "", element: <Home /> },
+    { path: "/login", element: <Login /> },
+    { path: "/register", element: <Register /> },
+  ])
+
   return (
-    <div>
-      Hai
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
